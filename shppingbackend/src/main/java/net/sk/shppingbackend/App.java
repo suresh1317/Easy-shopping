5f8e8b0edcab5ad18ff5fea42de4a8b95c888304
suresh kumawat <sureshkumawat077@gmail.com>
2018-12-04 23:22:28 +0530

    project setup done.

---
+++ shppingbackend/src/main/java/net/sk/shppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.sk.shppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
